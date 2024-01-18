a4638a66bd6cae11cbee08c14636ab5b814e76ea
Чернев Максим <cmd@coolclever.ru>
2024-01-18 16:36:53 +0300

    add swither icons

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,23 +1,28 @@
-import React from 'react';
-import {classNames} from "shared/lib/classNames/classNames";
-import cls from './Navbar.module.scss';
-import {AppLink, AppLinkTheme} from "shared/ui/AppLink/AppLink";
+import { classNames } from "shared/lib/classNames/classNames";
+import cls from "./Navbar.module.scss";
+import { AppLink, AppLinkTheme } from "shared/ui/AppLink/AppLink";
+import { ThemeSwitcher } from "widgets/ThemeSwitcher";
 
 interface NavbarProps {
-    className?: string;
+  className?: string;
 }
 
-export const Navbar = ({className}: NavbarProps) => {
-    return (
-        <div className={classNames (cls.navbar, {}, [className])}>
-            <div className={cls.links}>
-                <AppLink theme={AppLinkTheme.SECONDARY} to={'/'} className={cls.mainLink}>
-                    Главная
-                </AppLink>
-                <AppLink theme={AppLinkTheme.RED} to={'/about'}>
-                    О сайте
-                </AppLink>
-            </div>
-        </div>
-    );
+export const Navbar = ({ className }: NavbarProps) => {
+  return (
+    <div className={classNames(cls.navbar, {}, [className])}>
+      <ThemeSwitcher />
+      <div className={cls.links}>
+        <AppLink
+          theme={AppLinkTheme.SECONDARY}
+          to={"/"}
+          className={cls.mainLink}
+        >
+          Главная
+        </AppLink>
+        <AppLink theme={AppLinkTheme.RED} to={"/about"}>
+          О сайте
+        </AppLink>
+      </div>
+    </div>
+  );
 };
---
+++ src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -0,0 +1,19 @@
+import { classNames } from "shared/lib/classNames/classNames";
+import styles from "./ThemeSwitcher.module.scss";
+import { useTheme } from "app/providers/ThemeProvider";
+
+interface ThemeSwitcherProps {
+  className?: string;
+}
+export const ThemeSwitcher = ({ className }: ThemeSwitcherProps) => {
+  const { theme, toggleTheme } = useTheme();
+
+  return (
+    <button
+      className={classNames(styles.themeSwitcher, {}, [className])}
+      onClick={toggleTheme}
+    >
+      TOGGLE
+    </button>
+  );
+};
