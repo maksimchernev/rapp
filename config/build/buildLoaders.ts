2b9ce15df6f4a13810718084b1f4d50752015754
Чернев Максим <cmd@coolclever.ru>
2024-03-26 15:09:08 +0300

    add i18n extract plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -23,7 +23,23 @@ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
         ],
     }
 
-    // Если не используем тайпскрипт - нужен babel-loader
+    const babelLoader = {
+        test: /\.(ts|js|jsx|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env'],
+            "plugins": [
+                ["i18next-extract", {
+                    locales: ['ru', 'en'],
+                    keyAsDefaultValue: true
+                }]
+            ]
+          }
+        }
+      }
+
     const typescriptLoader = {
         test: /\.tsx?$/,
         use: 'ts-loader',
@@ -47,6 +63,7 @@ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
     return [
         fileLoader,
         svgLoader,
+        babelLoader,
         typescriptLoader,
         cssLoader,
     ]
