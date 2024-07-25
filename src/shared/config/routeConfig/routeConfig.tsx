00df126eb3c5d6339dc446156afa396712585652
Чернев Максим <cmd@coolclever.ru>
2024-07-25 15:14:45 +0300

    loader comp CI

--- src/shared/config/routeConfig/routeConfig.tsx
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -1,24 +1,31 @@
 import { type RouteProps } from "react-router-dom";
 import { MainPage } from "pages/MainPage";
 import { AboutPage } from "pages/AboutPage";
+import { NotFoundPage } from "pages/NotFoundPage";
 
 export enum AppRoutes {
 	MAIN = "main",
 	ABOUT = "about",
+	NOTFOUND = "notfound"
 }
 
 export const RoutePath: Record<AppRoutes, string> = {
 	[AppRoutes.MAIN]: "/",
 	[AppRoutes.ABOUT]: "/about",
+	[AppRoutes.NOTFOUND]: "*"
 };
 
 export const routeConfig: Record<AppRoutes, RouteProps> = {
 	[AppRoutes.MAIN]: {
 		path: RoutePath.main,
-		element: <MainPage />,
+		element: <MainPage />
 	},
 	[AppRoutes.ABOUT]: {
 		path: RoutePath.about,
-		element: <AboutPage />,
+		element: <AboutPage />
 	},
+	[AppRoutes.NOTFOUND]: {
+		path: RoutePath.notfound,
+		element: <NotFoundPage />
+	}
 };
