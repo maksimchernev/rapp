6175c361658f671a464d121617055d5a71bce3d7
Чернев Максим <cmd@coolclever.ru>
2024-07-25 13:11:14 +0300

    finish jest CI

--- src/shared/lib/classNames/classNames.test.ts
+++ src/shared/lib/classNames/classNames.test.ts
@@ -1,7 +1,19 @@
 import cn from "./classNames";
 
 describe("classNames", () => {
-	test("test", () => {
+	test("1 param", () => {
 		expect(cn("someClass")).toBe("someClass");
+	});
+	test("2 params", () => {
+		const expected = "someClass ccc c1";
+		expect(cn("someClass", {}, ["ccc", "c1"])).toBe(expected);
+	});
+	test("3 params", () => {
+		const expected = "someClass hover ccc c1";
+		expect(cn("someClass", { hover: true }, ["ccc", "c1"])).toBe(expected);
+	});
+	test("with null n undefined", () => {
+		const expected = "someClass ccc c1";
+		expect(cn(null, undefined, "someClass", {}, ["ccc", "c1"])).toBe(expected);
 	});
 });
--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -3,14 +3,16 @@ type Mods = Record<string, boolean | string>;
 export default function cn (...args: Array<string | Mods | string[]>): string {
 	const result = [];
 	for (const arg of args) {
-		if (typeof arg === "string" || typeof arg === "number") {
-			result.push(arg);
+		if (arg === null || arg === undefined) {
+			continue;
 		} else if (Array.isArray(arg)) {
 			result.push(...arg.filter(Boolean));
 		} else if (typeof arg === "object") {
 			result.push(...Object.entries(arg)
 				.filter(([className, value]) => Boolean(value))
 				.map(([className]) => className));
+		} else {
+			result.push(arg);
 		}
 	}
 	return result.join(" ");
